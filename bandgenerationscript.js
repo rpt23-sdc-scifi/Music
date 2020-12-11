4850c31e7deb2ef6d50a0b1c23a6fe017f59a2da
David Chege <dkc.ushift@gmail.com>
2020-12-11 17:18:56 -0600

    All generation scripts for tables completed

--- bandgenerationscript.js
+++ bandgenerationscript.js
@@ -4,23 +4,21 @@ const argv = require('yargs').argv
 
 
 const lines = argv.lines || 10000000
-const filename = argv.output || 'musicdescription.csv'
+const filename = argv.output || 'banddata.csv'
 const writeStream = fs.createWriteStream(filename)
 
 
 
 const createPost = (i) => {
-  const id = i
+  const band_id = i
   // id can be the position
-  const name = faker.lorem.words(2)
-  const length= faker.random.number(10)
-  const band_id =faker.random.number(500)
+  const name = faker.lorem.words(1)
 // we want each line to include an id ,n name, length, image, url, band_id
-  return `${id},${name},${length},${band_id}\n`
+  return `${band_id},${name}\n`
 }
 
 const startWriting = (writeStream, encoding, done) => {
-    writeStream.write(`id,name,length,band_id\n`, 'utf-8')
+    writeStream.write(`band_id,name\n`, 'utf-8')
   let i = lines
   function writing(){
     let canWrite = true
@@ -44,11 +42,4 @@ startWriting(writeStream, 'utf-8', () => {
   writeStream.end()
 })
 
-
-// should initialize stream and with all the above and startwriting
-// node musicgenerationscript.js --lines 10000000 --output musicdescription.csv
-
-
-
-//terminal import
-//psql -U postgres -d 'music' -c "\copy songdescription(id,name,length,url,image,band_id) FROM 'musicdata.csv' delimiter ',' csv header"
+//node bandgenerationscript.js --lines 10000000 --output banddata.csv
