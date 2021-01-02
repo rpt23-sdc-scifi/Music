4c293a19b8c9a23565aee5478b70273cf9bc4cc7
David Chege <dkc.ushift@gmail.com>
2021-01-02 16:52:07 -0600

    All API routes refactored for old api code

--- server/index.js
+++ server/index.js
@@ -35,17 +35,6 @@ app.get('/songdata/:id', async (req, res) => {
   }
 });
 
-app.post('/songdata/new', async (req, res) => {
-  try {
-    var dataToBeSaved = req.body;
-    console.log(req.body)
-    var saved = await songData.saveSong(dataToBeSaved)
-    res.send(saved);
-  } catch (error) {
-    console.log(error)
-  }
-});
-
 app.delete('/songdata/:id', async (req, res) => {
   try {
     var id = req.params.id;
@@ -58,7 +47,7 @@ app.delete('/songdata/:id', async (req, res) => {
   } catch (error) {
     console.log(error)
   }
-});
+})
 
 app.delete('/all', async (req, res) => {
   try {
@@ -72,21 +61,7 @@ app.delete('/all', async (req, res) => {
   } catch (error) {
     console.log(error)
   }
-});
-
-app.put('/songdata/:id', async (req, res) => {
-  try {
-    var id = req.params.id;
-    if (id > 100 || id < 0) {
-      res.end('SONG DOES NOT EXIST');
-    } else {
-      // var deletedSongByID = await songData.changeSong(id, name, length, url, image, band)
-      res.send(req.body);
-    }
-  } catch (error) {
-    console.log(error)
-  }
-});
+})
 
 app.get('/songsByBandID/:id', async (req, res) => {
   try {
@@ -101,6 +76,37 @@ app.get('/songsByBandID/:id', async (req, res) => {
     console.log(error);
   }
 })
+
+// all data must be in json string format
+app.post('/songdata/new', async (req, res) => {
+  try {
+    var dataToBeSaved = req.body;
+    console.log(req.body)
+    var saved = await songData.saveSong(dataToBeSaved)
+    res.send(saved);
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+app.put('/songdata/:id', async (req, res) => {
+  try {
+    var id = req.params.id;
+    var name = req.body.songName
+    var length = req.body.songLength
+    var url = req.body.songURL
+    var image = req.body.songImage
+    var band = req.body.bandID
+    if (id > 100 || id < 0) {
+      res.end('SONG DOES NOT EXIST');
+    } else {
+      var changeSong= await songData.changeSong(id, name, length, url, image, band)
+      res.send(changeSong);
+    }
+  } catch (error) {
+    console.log(error)
+  }
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
