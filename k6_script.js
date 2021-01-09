87f5b920c4200e86e6731dfb2cbbb0028b307224
David Chege <dkc.ushift@gmail.com>
2021-01-09 06:35:00 -0600

    Added bashing script for k6 test and new threshold

--- k6_script.js
+++ k6_script.js
@@ -1,25 +1,22 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+
+import { check, sleep } from 'k6';
+
+export let options = {
+  thresholds : {
+       'http_req_duration': ['p(90) < 500']
+
+  },
+  stages: [
+    { duration: '120s', target: 3500 }
+  ],
+  startVUs: 2000
+};
 export default function () {
-  http.get('http://test.k6.io');
+  let max = 10000000;
+  let min = 9000000;
+  let randomId = Math.random() * (max - min) + min;
+  let res = http.get(`http://localhost:3005/api/song/${randomId}`);
+  check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
-
-// import http from 'k6/http';
-
-// import { check, sleep } from 'k6';
-
-// export let options = {
-//   stages: [
-//     { duration: '120s', target: 3500 }
-//   ],
-//   startVUs: 2000
-// };
-// export default function () {
-//   let max = 10000000;
-//   let min = 9000000;
-//   let randomId = Math.random() * (max - min) + min;
-//   let res = http.get(`http://localhost:3005/${randomId}`);
-//   check(res, { 'status was 200': (r) => r.status == 200 });
-//   sleep(1);
-// }
---
+++ stresstest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# run-test.sh
+k6 run k6_script.js
+if [ $? -ne 0 ]; then
+  currentDate=`date +"%D %T"`
+  mail -s "🚨 Failed performance test" dkc.ushift@mail.com <<< "${currentDate} - My performance test"
+  exit 1
+fi
