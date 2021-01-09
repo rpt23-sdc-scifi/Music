23f06dd8cdcfc07a458eff8214946b9195da5d4b
David Chege <dkc.ushift@gmail.com>
2021-01-09 07:13:05 -0600

    Fixing thresholds

--- k6_script.js
+++ k6_script.js
@@ -4,17 +4,28 @@ import { check, sleep } from 'k6';
 
 export let options = {
   thresholds : {
-       'http_req_duration': ['p(90) < 500']
+       'http_req_duration': [{threshold: 'p(90) < 500', abortOnFail: true, delayAbortEval: '5s' }]
 
+      
   },
   stages: [
-    { duration: '120s', target: 3500 }
+    { duration: '5s', target: 1 },
+    { duration: '10s', target: 10 },
+    { duration: '10s', target: 100 },
+    { duration: '15s', target: 500 },
+    { duration: '15s', target: 1000 },
+    { duration: '10s', target: 100 }
+    
   ],
-  startVUs: 2000
+//   //maximum requests per second
+//   rps: 100
+
+//virtual user start
+//   startVUs: 1
 };
 export default function () {
-  let max = 10000000;
-  let min = 9000000;
+  let max = 9999999;
+  let min = 0;
   let randomId = Math.random() * (max - min) + min;
   let res = http.get(`http://localhost:3005/api/song/${randomId}`);
   check(res, { 'status was 200': (r) => r.status == 200 });
--- server/controller.js
+++ server/controller.js
@@ -4,7 +4,7 @@ const db = require('../database/postgres_db.js');
 const songs = async (req,res) => {
  try {
     const id = req.params.id;
-    if (id > 1000000 || id < 0) {
+    if (id > 10000000  || id < 0) {
       res.end('SONG DOES NOT EXIST');
     } else {
       const Song = await db.findSong(id)
@@ -25,7 +25,7 @@ const update = async (req,res) => {
         var url = req.body.songURL
         var image = req.body.songImage
         var band_id = req.body.bandID
-        if (id > 1000000 || id < 0) {
+        if (id > 10000000 || id < 0) {
           res.end('SONG DOES NOT EXIST');
         } else {
           const changeSong = await db.updateSong(songname, length, url, image, band_id, id)
@@ -47,13 +47,8 @@ const insert = async (req,res) => {
         var url = req.body.songURL
         var image = req.body.songImage
         var band_id = req.body.bandID
-        if (id > 1000000 || id < 0) {
-          res.end('SONG DOES NOT EXIST');
-        } else {
-          const addSong = await db.insertSong(songname, length, url, image, band_id, id)
-          res.send(addSong);
-        
-        }
+        const addSong = await db.insertSong(songname, length, url, image, band_id, id)
+        res.send(addSong);
       } catch (error) {
         console.log(error)
       }
@@ -63,7 +58,7 @@ const insert = async (req,res) => {
 const deleter = async (req,res) => {
     try {
         const id = req.params.id;
-        if (id > 1000000 || id < 0) {
+        if (id > 10000000 || id < 0) {
           res.end('SONG DOES NOT EXIST');
         } else {
           const deleteSong = await db.deleteSong(id)
